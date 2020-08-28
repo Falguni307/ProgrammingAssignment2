557adb92b5b4ef1ab590a5e2d2c3662cba471791
Falguni307 <pfalguni307@gmail.com>
2020-08-28 11:07:40 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is the edited code. Last commit on this code was done on 27-8-2020
+## This function creates a special "matrix" object that can cache its inverse.
+## smat is the matrix object that user will submit on the console
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-inv <- NULL
-set <- function(y) {
-x <<- y
-inv <<- NULL
+makeCacheMatrix <- function(smat = matrix()) {
+    invsmat <- NULL
+    set <- function(x) {
+        smat <<- x
+        invsmat <<- NULL
+    }
+    get <- function() smat
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() invsmat
+    list(set = set,
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
-get <- function() x
-setInverse <- function(inverse) inv <<- inverse
-getInverse <- function() inv
-list(set = set,
-get = get,
-setInverse = setInverse,
-getInverse = getInverse)
-}
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" created by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed), then it should retrieve the inverse from the cache.
 
-makeCacheMatrix <- function(x = matrix()) {
-
-inv <- NULL
-set <- function(y) {
-x <<- y
-inv <<- NULL
-}
-get <- function() x
-setinv <- function(inverse) inv <<- inverse
-getinv <- function() inv
-list(set = set, get = get, setinv = setinv, getinv = getinv)
+cacheSolve <- function(smat, ...) {
+    ## Return a matrix that is the inverse of 'smat'
+    inv <- smat$getInverse()
+    if (!is.null(inv)) {
+        message("getting cached data")
+        return(invsmat)
+    }
+    mat <- smat$get()
+    invsmat <- solve(mat, ...)
+    smat$setInverse(invsmat)
+    invsmat
 }
